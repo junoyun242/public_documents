3f14884cabcbdf9de6a72645ebec4bfa60647a30
Juno Yun <junoyun242@gmail.com>
2024-06-28 01:40:50 +0900

    React revealjs test

--- ppt/react-reveal-test/src/vite.config.ts
+++ ppt/react-reveal-test/src/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://junoyun242.github.io/public_documents/ppt/react-reveal-test",
+});
